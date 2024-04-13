03ac7b749dfab7df6ad0052f76e94493743c6154
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2024-04-13 15:50:30 -0300

    update...

---
+++ my-ip/src/main.rs
@@ -0,0 +1,33 @@
+use reqwest;
+//use serde_json;
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // KNOWN RETURN - HashMap
+    const URL1: &str = "https://httpbin.org/ip";
+    match reqwest::get(URL1).await {
+        Ok(resp) => {
+            let json = resp.json::<HashMap<String, String>>().await?;
+            println!("{}", json["origin"])
+        }
+        Err(err) => {
+            println!("Reqwest Error: {}", err)
+        }
+    }
+
+    // UNKNOWN RETURN - serde_json::Value
+    // const URL1: &str = "https://dummyjson.com/products/1";
+    // match reqwest::get(URL1).await {
+    //     Ok(resp) => {
+    //         let json: serde_json::Value = resp.json().await?;
+    //         println!("{:?}", json)
+    //     }
+    //     Err(err) => {
+    //         println!("Reqwest Error: {}", err)
+    //     }
+    // }
+
+    Ok(())
+}
